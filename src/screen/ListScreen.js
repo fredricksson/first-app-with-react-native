9723c51fc583b6df7f31f338b07d2d661caa250e
fredricksson <bandefredrickson@gmail.com>
2021-10-05 10:23:08 +0200

    flatList

--- src/screen/ListScreen.js
+++ src/screen/ListScreen.js
@@ -3,21 +3,27 @@ import { Text, StyleSheet, View, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        { name: 'freddricksson 2' },
-        { name: 'freddricksson 2'},
-        { name: 'freddricksson 3'},
-        { name: 'freddricksson 4'},
-        { name: 'freddricksson 6'},
-        { name: 'freddricksson 5'},
+        { name: 'freddricksson 2' , age: 12},
+        { name: 'freddricksson 2' , age: 12},
+        { name: 'freddricksson 3' , age: 12},
+        { name: 'freddricksson 4' , age: 12},
+        { name: 'freddricksson 6' , age: 12},
+        { name: 'freddricksson 5' , age: 12},
     ]
-    return <FlatList 
+    /*return <FlatList 
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 keyExtractor={(friend) => friend.name}
                 data={friends} 
                 renderItem={ ( { item } )=> {
         return <Text style={styles.textStyle}> {item.name} </Text>
-    }}/>
+    }}/>*/
+    return <FlatList
+            data={friends}
+            renderItem={ ({item}) => {
+                return <Text> {item.name} - {item.age} </Text>
+            }}
+    />
 }
 
 const styles = StyleSheet.create({
