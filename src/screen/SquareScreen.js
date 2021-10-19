597fb32cc1c4a6d7ee9c28ec42a16c8a1d3195f5
fredricksson <bandefredrickson@gmail.com>
2021-10-19 07:37:25 +0200

    reducers

--- src/screen/SquareScreen.js
+++ src/screen/SquareScreen.js
@@ -1,28 +1,67 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import ColorCounter from './components/ColorCounter'
+
+const COLOR_INCREMENT = 15;
+const reducer = (state, action) => {
+    // state === { red: number, green: number, blue: number }
+    // action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15}
+    switch (action.type) {
+        case 'change_red':
+            // above we have something similar { red: 0, green: 0, blue: 0, red: state.red + action.amount}
+            // it will overwrite the value of red
+            return state.red + action.payload > 255 || state.red + action.payload < 0
+             ? state
+             : { ...state, red: state.red + action.payload }
+        case 'change_green':
+            // above we have something similar { red: 0, green: 0, blue: 0, green: state.green + action.payload}
+            // it will overwrite the value of green
+            return state.green + action.payload > 255 || state.green + action.payload < 0 ? state : { ...state, green: state.green + action.payload }
+
+        case 'change_blue':
+            // above we have something similar { red: 0, green: 0, blue: 0, red: state.blue + action.payload}
+            // it will overwrite the value of blue
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 ? state : { ...state, blue: state.blue + action.payload }
+        
+        default:
+            return state
+
+    }
+};
+
 const SquareScreen = () => {
-    const [red, setRed] = useState(0)
-    const [green, setGreen] = useState(0)
-    const [blue, setBlue] = useState(0)
+    // dispatch can be anything like runMyReducer, but the convenction is dispatch
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+    
     return (
         <View>
             <ColorCounter 
-                onIncrease={ () => setRed(red + 1)} 
-                onDecrease={ () => setRed(red - 1)}
-                color="Red"
+                onIncrease={ () => { 
+                   dispatch({ type: 'change_red', payload: COLOR_INCREMENT})
+                 }}
+                onDecrease={ () => {
+                    dispatch({ type: 'change_red', payload: -1*COLOR_INCREMENT})
+                } 
+                    
+                } 
+                
+                color='red'
             />
             <ColorCounter
                 color="Blue"
-                onIncrease={ () => setBlue(blue + 1)} 
-                onDecrease={ () => setBlue(blue - 1)}
+                onIncrease={ () => {
+                    dispatch({ type: 'change_blue', payload: COLOR_INCREMENT})
+                }} 
+                onDecrease={ () => {
+                    dispatch({ type: 'change_blue', payload: -1*COLOR_INCREMENT})
+                }}
             />
             <ColorCounter
                 color="Green"
-                onIncrease={ () => setGreen(green + 1)} 
-                onDecrease={ () => setGreen(green - 1)}
+                onIncrease={ () => dispatch({ type: 'change_green', payload: COLOR_INCREMENT}) } 
+                onDecrease={ () => dispatch({ type: 'change_green', payload: -1*COLOR_INCREMENT}) }
              />
-             <View style={{height: 150, width: 150, backgroundColor: `rgb(${red},${green}, ${blue})`}}/>
+             <View style={{height: 150, width: 150, backgroundColor: `rgb(${state.red},${state.green}, ${state.blue})`}}/>
         </View>
     )
 }
--- src/screen/components/ColorCounter.js
+++ src/screen/components/ColorCounter.js
@@ -5,11 +5,13 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 const ColorCounter = ( { color, onIncrease, onDecrease } ) => {
 
 
-    return (<View>
+    return (
+    <View>
         <Text>{color}</Text>
         <Button onPress={() => onIncrease()} title={`Increase  ${color}`}/>
-        <Button OnPress={() => onDecrease()} title={`Decrease ${color}`}/>
-    </View>)
+        <Button onPress={() => onDecrease()}   title={`Decrease ${color}`}/>
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
